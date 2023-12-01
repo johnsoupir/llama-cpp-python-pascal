e6b7607dfa25decd6c174c5254646748c1254566
John Soupir <johnsoupir@gmail.com>
2023-11-30 19:24:09 -0600

    Makes install.sh reinstall

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CMAKE_ARGS="--DLLAMA_CUBLAS=on -DLLAMA_CUDA_FORCE_MMQ=ON" pip install llama-cpp-python
+CMAKE_ARGS="--DLLAMA_CUBLAS=on -DLLAMA_CUDA_FORCE_MMQ=ON" pip install -e .[all] --upgrade --force-reinstall --no-cache-dir
