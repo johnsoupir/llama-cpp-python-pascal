5e2b8ad7d36f5f57d6aa150dbbe02e1ca37d8890
John Soupir <johnsoupir@gmail.com>
2023-11-30 19:50:48 -0600

    Adds pascal installer shell script

--- installForPascal.sh
+++ installForPascal.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+#Set CMAKE arguments for cuBLAS and force MMQ (2x boost on pascal). Then reinstall and overwrite any old version.
 CMAKE_ARGS="-DLLAMA_CUBLAS=on -DLLAMA_CUDA_FORCE_MMQ=ON" pip install -e .[all] --upgrade --force-reinstall --no-cache-dir
